243dff4af205153bda0416c76389c5234825edef
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-08 11:20:41 +0300

    Added assertation in test

--- src/SplayTreeTest.java
+++ src/SplayTreeTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SplayTreeTest {
     @Test
     void testSplayTree() {
-        SplayTree<Double> tree = new SplayTree<Double>(new DoublesComparator());
+        SplayTree<Double> tree = new SplayTree<>(new DoublesComparator());
         tree.add(1.0);
         tree.add(2.0);
         System.out.println(tree.print());
@@ -18,5 +18,6 @@ class SplayTreeTest {
         tree.add(8.0);
         tree.add(9.0);
         System.out.println(tree.print());
+        assertEquals("[1.0, 2.0, 3.0, 4.0, 5.0, 6.0. 7.0, 8.0, 9.0]", tree.print());
     }
 }
